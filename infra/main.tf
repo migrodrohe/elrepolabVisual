cc3913c5255df32c4c21b0d0938054e89a9ac5d4
migrodrohe <migrodrohe12@gmail.com>
2025-10-17 21:45:10 -0500

    cualquiercosa

--- infra/main.tf
+++ infra/main.tf
@@ -2,8 +2,7 @@
 resource "aws_dynamodb_table" "mytable" {
   name = var.table_name
   billing_mode   = "PROVISIONED"
-  read_capacity  = 20
-  write_capacity = 20
+
   hash_key       = "username"
   range_key      = "last_name"
 
